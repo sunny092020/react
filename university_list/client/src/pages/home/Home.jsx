3ffa6471e425c84ac1e4326df599d59570a1820e
Vu Hoang Giang <itworkjp@gmail.com>
2021-08-10 05:34:15 +0700

    simplify home

--- university_list/client/src/pages/home/Home.jsx
+++ university_list/client/src/pages/home/Home.jsx
@@ -1,13 +1,9 @@
-import Topbar from "../../components/topbar/Topbar";
 import "./home.css"
 
 export default function Home() {
   return (
-    <>
-      <Topbar />
-      <div className="homeContainer">
-        Home
-      </div>
-    </>
+    <div className="homeContainer">
+      Home
+    </div>
   );
 }
