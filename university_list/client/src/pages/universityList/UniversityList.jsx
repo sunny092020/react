cc690d07a90f1ec1a9d2e47324d889d1fdf98c4f
Vu Hoang Giang <itworkjp@gmail.com>
2021-08-10 10:13:43 +0700

    add filter

--- university_list/client/src/pages/universityList/UniversityList.jsx
+++ university_list/client/src/pages/universityList/UniversityList.jsx
@@ -1,9 +1,106 @@
 import "./universityList.css";
 import { DataGrid } from "@material-ui/data-grid";
 import { useState, useEffect } from "react";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import TextField from '@material-ui/core/TextField';
+
+import ClearIcon from '@material-ui/icons/Clear';
+import SearchIcon from '@material-ui/icons/Search';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/styles';
+
+import {
+  GridToolbarDensitySelector,
+  GridToolbarFilterButton,
+} from '@material-ui/data-grid';
+
+function escapeRegExp(value) {
+  return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+
+const defaultTheme = createMuiTheme();
+const useStyles = makeStyles(
+  (theme) => ({
+    root: {
+      padding: theme.spacing(0.5, 0.5, 0),
+      justifyContent: 'space-between',
+      display: 'flex',
+      alignItems: 'flex-start',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      [theme.breakpoints.down('xs')]: {
+        width: '100%',
+      },
+      margin: theme.spacing(1, 0.5, 1.5),
+      '& .MuiSvgIcon-root': {
+        marginRight: theme.spacing(0.5),
+      },
+      '& .MuiInput-underline:before': {
+        borderBottom: `1px solid ${theme.palette.divider}`,
+      },
+    },
+  }),
+  { defaultTheme },
+);
+
+function QuickSearchToolbar(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div>
+        <GridToolbarFilterButton />
+        <GridToolbarDensitySelector />
+      </div>
+      <TextField
+        variant="standard"
+        value={props.value}
+        onChange={props.onChange}
+        placeholder="Search…"
+        className={classes.textField}
+        InputProps={{
+          startAdornment: <SearchIcon fontSize="small" />,
+          endAdornment: (
+            <IconButton
+              title="Clear"
+              aria-label="Clear"
+              size="small"
+              style={{ visibility: props.value ? 'visible' : 'hidden' }}
+              onClick={props.clearSearch}
+            >
+              <ClearIcon fontSize="small" />
+            </IconButton>
+          ),
+        }}
+      />
+    </div>
+  );
+}
+
+QuickSearchToolbar.propTypes = {
+  clearSearch: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default function UniversityList() {
   const [data, setData] = useState([]);
+  const [searchText, setSearchText] = React.useState('');
+
+  const requestSearch = (searchValue) => {
+    setSearchText(searchValue);
+    const searchRegex = new RegExp(escapeRegExp(searchValue), 'i');
+    const filteredRows = data.filter((row) => {
+      return Object.keys(row).some((field) => {
+        return searchRegex.test(row[field]);
+      });
+    });
+    console.log(filteredRows);
+    setData(filteredRows);
+  };
 
   const getData=()=> { fetch("world_universities_and_domains.json").then((r) => r.json())
     .then((data) => {
@@ -41,11 +138,19 @@ export default function UniversityList() {
   return (
     <div className="universityList">
       <DataGrid
+        components={{ Toolbar: QuickSearchToolbar }}
         rows={data}
         disableSelectionOnClick
         columns={columns}
         pageSize={10}
         checkboxSelection
+        componentsProps={{
+          toolbar: {
+            value: searchText,
+            onChange: (event) => requestSearch(event.target.value),
+            clearSearch: () => requestSearch(''),
+          },
+        }}
       />
     </div>
   );
