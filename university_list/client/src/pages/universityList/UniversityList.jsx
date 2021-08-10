c2560c8697939337eb42d556504a5ff8348828c2
Vu Hoang Giang <itworkjp@gmail.com>
2021-08-10 08:02:36 +0700

    display universities

--- university_list/client/src/pages/universityList/UniversityList.jsx
+++ university_list/client/src/pages/universityList/UniversityList.jsx
@@ -1,16 +1,39 @@
 import "./universityList.css";
 import { DataGrid } from "@material-ui/data-grid";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function UniversityList() {
+  const [data, setData] = useState([]);
+
+  const getData=()=> { fetch("world_universities_and_domains.json").then((r) => r.json())
+    .then((data) => {
+        console.log("finish fetching");
+        setData(data.map((currElement, index) => {
+            currElement.id = index+1;
+            return currElement; //equivalent to list[index]
+          })
+        );
+    })
+  };
+
+  useEffect(()=>{
+    getData()
+  },[])
+
+  const columns = [
+    { field: "id", headerName: "No", width: 100 },
+    { field: "name", headerName: "Name", width: 600 },
+    { field: "country", headerName: "Country", width: 500 },
+  ];
+
   return (
-    <div className="userList">
+    <div className="universityList">
       <DataGrid
-        rows={[]}
+        rows={data}
         disableSelectionOnClick
-        columns={[]}
-        pageSize={8}
+        columns={columns}
+        pageSize={10}
         checkboxSelection
       />
     </div>
