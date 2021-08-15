cca6c6f6b06336e2c71d11e724d9d3d04cb58b86
Vu Hoang Giang <itworkjp@gmail.com>
2021-08-15 12:09:01 +0700

    add subscriptions api

--- university_list/api/routes/subscription.js
+++ university_list/api/routes/subscription.js
@@ -7,14 +7,10 @@ router.post("/subscribe", async (req, res) => {
 
     let rawdata = fs.readFileSync('subscriptions.json');
     let subscriptions = JSON.parse(rawdata);
-    console.log(subscriptions);
-
     if(!subscriptions.includes(email)) {
       subscriptions.push(email);
     }
-
     fs.writeFileSync('subscriptions.json', JSON.stringify(subscriptions));
-    
     res.status(200).json();
   } catch (err) {
     res.status(500).json(err)
--- university_list/client/src/pages/newsletter/Newsletter.jsx
+++ university_list/client/src/pages/newsletter/Newsletter.jsx
@@ -3,11 +3,21 @@ import { useRef } from "react";
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
 import React from 'react';
+import axios from "axios";
 
 export default function Newsletter() {
   const inputEl = useRef(null);
   const onButtonClick = () => {
     console.log(inputEl.current.value);
+    axios.post("/subscription/subscribe", {"email" : inputEl.current.value})
+    .then(function (response) {
+      console.log(response);
+      alert(`${inputEl.current.value} has been subscribed!`);
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert(`${inputEl.current.value} cannot be subscribed!`);
+    });
   };
   return (
     <div className="newsLetter">
