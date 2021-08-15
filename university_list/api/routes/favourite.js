467ca73ec6a5cdf96eace3f8bcdcd8f599d314e2
Vu Hoang Giang <itworkjp@gmail.com>
2021-08-15 19:21:10 +0700

    add favourite api

--- university_list/api/routes/favourite.js
+++ university_list/api/routes/favourite.js
@@ -1,9 +1,33 @@
 const router = require("express").Router();
 const fs = require('fs');
 
-router.post("/add", async (req, res) => {
+router.post("/toggle", async (req, res) => {
   try {
     username = req.body.username;
+    favourite = req.body.favourite;
+    university = req.body.university;
+
+    let rawdata = fs.readFileSync('users.json');
+    let users = JSON.parse(rawdata);
+
+    let favourites = users[username].favourites;
+    console.table(favourites);
+    console.log(`favourite ${favourite}`);
+
+    if (favourite) {
+      if (!favourites.includes(university)) {
+        favourites.push(university);
+      }
+    } else {
+      favourites = favourites.filter(u => u !== university);
+    }
+
+    users[username].favourites = favourites
+
+    console.table(users);
+
+    fs.writeFileSync('users.json', JSON.stringify(users));
+
     res.status(200).json();
   } catch (err) {
     res.status(500).json(err)
