4332008ada142fde484f7a57072dadf7d59ea5c9
Vu Hoang Giang <itworkjp@gmail.com>
2021-08-21 17:44:32 +0700

    Arrow functions are an ES6 feature, but here you have an async arrow function.
    Async functions in general are an ES8 (or 2017) feature. Therefore you need to specify the following setting at the root of your config:
    
    parserOptions: {
      ecmaVersion: 8 // or 2017
    }
    This will let the parser know to expect the => token after async is used.

--- university_list/.eslintrc.js
+++ university_list/.eslintrc.js
@@ -23,6 +23,9 @@ module.exports = {
     es6: true,
     jquery: true,
   },
+  parserOptions: {
+    ecmaVersion: 8,
+  },
   rules: {
     // The rules below are listed in the order they appear on the eslint
     // rules page. All rules are listed to make it easier to keep in sync
