b08d90e555259cf7c0b333b9547671333d48eab4
Vu Hoang Giang <itworkjp@gmail.com>
2021-08-21 17:39:15 +0700

    fix lint

--- university_list/api/index.js
+++ university_list/api/index.js
@@ -1,23 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const authRoute = require("./routes/auth");
-const subscriptionRoute = require("./routes/subscription");
-const favouriteRoute = require("./routes/favourite.js");
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const authRoute = require('./routes/auth');
+const subscriptionRoute = require('./routes/subscription');
+const favouriteRoute = require('./routes/favourite.js');
 
 dotenv.config();
 
-//middleware
+// middleware
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("common"));
+app.use(morgan('common'));
 
-app.use("/api/auth", authRoute);
-app.use("/api/subscription", subscriptionRoute);
-app.use("/api/favourites", favouriteRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/subscription', subscriptionRoute);
+app.use('/api/favourites', favouriteRoute);
 
 app.listen(8800, () => {
-  console.log("Backend server is running!");
 });
--- university_list/script/lint.sh
+++ university_list/script/lint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 ./node_modules/.bin/eslint api/*.js
+./node_modules/.bin/eslint api/routes/*.js
