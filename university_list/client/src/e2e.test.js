be706b0ba64d08accd77bfca74ab96ea6da15495
Vu Hoang Giang <itworkjp@gmail.com>
2021-08-22 20:28:48 +0700

    add login test

--- university_list/client/src/e2e.test.js
+++ university_list/client/src/e2e.test.js
@@ -6,13 +6,30 @@ describe('App.js', () => {
   let page;
 
   beforeAll(async () => {
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({headless: false});
     page = await browser.newPage();
-    await page.goto('https://google.com');
+    await page.goto('http://localhost:3000');
   });
 
-  it('should display "google" text on page', async () => {
-    await expect(page).toMatch('google');
+  it('should login', async () => {
+    await page.waitForSelector('input[placeHolder=Username]');
+    await page.$eval(
+        'input[placeHolder=Username]',
+        (el) => el.value = 'sunny',
+    );
+
+    await page.waitForSelector('input[placeHolder=Password]');
+    await page.$eval(
+        'input[placeHolder=Password]',
+        (el) => el.value = 'pass',
+    );
+
+    await page.click('button');
+
+    await page.waitForFunction(
+        // eslint-disable-next-line max-len
+        'document.querySelector("body").innerText.includes("Marywood University")',
+    );
   });
 
   afterAll(() => browser.close());
